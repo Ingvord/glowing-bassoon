751a569262ef045c9bb9e84d1d1c6f94f25c85a4
ingvord <ingvord.mail@gmail.com>
2024-02-20 13:31:50 +0100

    add initial interfaces and classes

---
+++ src/main/java/ru/ingvord/glowing/bassoon/Handler.java
@@ -0,0 +1,5 @@
+package ru.ingvord.glowing.bassoon;
+
+public interface Handler {
+    ApplicationStatusResponse performOperation(String id);
+}
---
+++ src/main/java/ru/ingvord/glowing/bassoon/HandlerImpl.java
@@ -0,0 +1,8 @@
+package ru.ingvord.glowing.bassoon;
+
+public class HandlerImpl implements Handler {
+    @Override
+    public ApplicationStatusResponse performOperation(String id) {
+        return null;
+    }
+}
