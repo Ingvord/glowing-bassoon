d593c069ab8898484ba5c6fbf4987407af4b319e
ingvord <ingvord.mail@gmail.com>
2024-02-20 14:45:14 +0100

    extract UpstreamService

--- src/main/java/ru/ingvord/glowing/bassoon/HandlerImpl.java
+++ src/main/java/ru/ingvord/glowing/bassoon/HandlerImpl.java
@@ -1,15 +1,13 @@
 package ru.ingvord.glowing.bassoon;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.time.Duration;
+import java.time.Instant;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 public class HandlerImpl implements Handler {
     private final Client client;
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);//TODO tune
-
 
     public HandlerImpl(Client client) {
         this.client = client;
@@ -17,28 +15,16 @@ public class HandlerImpl implements Handler {
 
     @Override
     public ApplicationStatusResponse performOperation(String id) {
-        var service1 = new Callable<Response>(){
-            @Override
-            public Response call() {
-                return client.getApplicationStatus1(id);
-            }
-        };
+        var service1 = new UpstreamService(id, client::getApplicationStatus1, Executors.newScheduledThreadPool(1));
 
-        var service2 = new Callable<Response>(){
-            @Override
-            public Response call() {
-                return client.getApplicationStatus2(id);
-            }
-        };
+        var service2 = new UpstreamService(id, client::getApplicationStatus2, Executors.newScheduledThreadPool(1));
 
 
-        var future1 = CompletableFuture.supplyAsync(service1::call);
-        var future2 = CompletableFuture.supplyAsync(service2::call);
+        var future1 = CompletableFuture.supplyAsync(service1::callServiceWithRetry);
+        var future2 = CompletableFuture.supplyAsync(service2::callServiceWithRetry);
 
-        var status1 = future1.thenCompose(handleRetryResponse(service1));
-        var status2 = future2.thenCompose(handleRetryResponse(service1));
 
-        CompletableFuture<Response> anyOf = CompletableFuture.anyOf(status1, status2)
+        CompletableFuture<Response> anyOf = CompletableFuture.anyOf(future1, future2)
                 .completeOnTimeout(new ApplicationStatusResponse.Failure(null, 1),15, TimeUnit.SECONDS)
                 .thenApply(response -> (Response) response);
 
@@ -49,38 +35,70 @@ public class HandlerImpl implements Handler {
                 return new ApplicationStatusResponse.Success(success.applicationId(), success.applicationStatus());
             } else if (response instanceof Response.Failure) {
                 Response.Failure failure = (Response.Failure) response;
-                return new ApplicationStatusResponse.Failure(null, 1); // Simplified for example purposes
+                return getFailure(service1, service2);
             }
         } catch (InterruptedException | ExecutionException e) {
             // Handle execution exceptions here
-            return new ApplicationStatusResponse.Failure(null, 1);
+            return getFailure(service1, service2);
         }
 
         // Fallback, in case no conditions are met
-        return new ApplicationStatusResponse.Failure(null, 0);
+        return getFailure(service1, service2);
     }
 
-    @NotNull
-    private Function<Response, CompletionStage<Response>> handleRetryResponse(Callable<Response> service) {
-        return response -> {
-            if (response instanceof Response.RetryAfter) {
-                Response.RetryAfter retryAfter = (Response.RetryAfter) response;
-                return delay(retryAfter.delay().toMillis()).thenCompose(v -> CompletableFuture.supplyAsync(() -> {
-                    try {
-                        return service.call();
-                    } catch (Exception e) {
-                        return new Response.Failure(e);
-                    }
-                }));
+    private ApplicationStatusResponse getFailure(UpstreamService service1, UpstreamService service2){
+        return new ApplicationStatusResponse.Failure(
+                Duration.between(
+                        Instant.ofEpochMilli(Math.max(service1.lastRequestTime.toEpochMilli(), service2.lastRequestTime.toEpochMilli())),
+                        Instant.now()), Math.max(service1.getRetriesCount(), service2.getRetriesCount())); // Simplified for example purposes
+    }
+
+    private class UpstreamService {
+        private final String id;
+        private final Function<String, Response> serviceFunction;
+        private final ScheduledExecutorService scheduler;
+        private final AtomicInteger retriesCount = new AtomicInteger(0);
+        private Instant lastRequestTime;
+
+        public UpstreamService(String id, Function<String, Response> serviceFunction, ScheduledExecutorService scheduler) {
+            this.id = id;
+            this.serviceFunction = serviceFunction;
+            this.scheduler = scheduler;
+        }
+
+        public CompletableFuture<Response> callServiceWithRetry() {
+            return CompletableFuture.supplyAsync(this::callService)
+                    .thenCompose(this::handleResponse);
+        }
+
+        private Response callService() {
+            lastRequestTime = Instant.now();
+            retriesCount.incrementAndGet();
+            return serviceFunction.apply(id);
+        }
+
+        private CompletableFuture<Response> handleResponse(Response response) {
+            if (response instanceof Response.RetryAfter retryAfter) {
+                return delay(retryAfter.delay().toMillis())
+                        .thenCompose(v -> callServiceWithRetry());
             } else {
                 return CompletableFuture.completedFuture(response);
             }
-        };
+        }
+
+        private CompletableFuture<Void> delay(long delay) {
+            CompletableFuture<Void> future = new CompletableFuture<>();
+            scheduler.schedule(() -> future.complete(null), delay, TimeUnit.MILLISECONDS);
+            return future;
+        }
+
+        public int getRetriesCount() {
+            return retriesCount.get();
+        }
+
+        public Instant getLastRequestTime() {
+            return lastRequestTime;
+        }
     }
 
-    private CompletableFuture<Void> delay(long delay) {
-        CompletableFuture<Void> future = new CompletableFuture<>();
-        scheduler.schedule(() -> future.complete(null), delay, TimeUnit.MILLISECONDS);
-        return future;
-    }
 }
